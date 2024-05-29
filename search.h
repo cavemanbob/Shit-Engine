af1fe6e29378a22f852b9210ad32f4b1d902b1e2
Efe Burak Ostundag <129344217+cavemanbob@users.noreply.github.com>
2024-05-30 01:58:37 +0300

    MOVE ORDER ADDED !!
    ------- NONE ---------------
    position r3kb1r/pp1n1ppp/4pn2/q1pp4/8/2N1PN2/PPPPKPPP/R1BQ3R w kq - 4 6
    go 6
    info depth 6
    info nodes 8263072
    bestmove e2f1
    info pv e2f1  a5b4  d1e1  b4c4  f1g1  c4c3
    info string 0.842sec
    position r3kb1r/pp1n1ppp/4pn2/q1pp4/8/2N1PN2/PPPPKPPP/R1BQ3R w kq - 4 6
    go 7
    info depth 7
    info nodes 71879593
    bestmove d1f1
    info pv d1f1  d5d4  e3d4  c5d4  f3d4  a5e5  e2f3
    info string 7.724sec
    -------------- MOVE ORDER --------------
    position r3kb1r/pp1n1ppp/4pn2/q1pp4/8/2N1PN2/PPPPKPPP/R1BQ3R w kq - 4 6
    go 6
    info depth 6
    info nodes 6199859
    bestmove d1e1
    info pv d1e1  a5b4  e2f1  b4c4  f1g1  c4c3
    info string 0.74sec
    position r3kb1r/pp1n1ppp/4pn2/q1pp4/8/2N1PN2/PPPPKPPP/R1BQ3R w kq - 4 6
    go 7
    info depth 7
    info nodes 29114112
    bestmove d1f1
    info pv d1f1  d5d4  e3d4  c5d4  f3d4  a5e5  e2f3
    info string 3.377sec

--- search.h
+++ search.h
@@ -7,18 +7,6 @@ int red_limit = 3;
 float alphabeta(position b, int depth, float alpha, float beta){
 	Node_Total++;
 	if(depth == 0){
-/*		const u8 side = (b.key >> 8 & 1ULL) ? WHITE : BLACK;
-		const u8 ENEMY_KING_SQUARE = (side == WHITE) ? Ls1bIndex(b.bk) : Ls1bIndex(b.wk);
-		const u8 ALLY_KING_SQUARE = (side == WHITE) ? Ls1bIndex(b.wk) : Ls1bIndex(b.bk);
-		//if(Node_Total > 450 && (is_square_attacked(b, ALLY_KING_SQUARE, (side)) || is_square_attacked(b, ENEMY_KING_SQUARE, SwapSide(side)))){
-			//ReadableBoard(b);
-			//_sleep(4500);
-		//}
-		//const u8 Current_Move = b.key & 0b1111111;
-		if( BitCheck(b.key, 33) == 0 && (is_square_attacked(b, ALLY_KING_SQUARE, side) || is_square_attacked(b, ENEMY_KING_SQUARE, SwapSide(side)) || BitCheck(b.key, 31)) ){
-			b.key |= 1ULL << 33;
-			return alphabeta(b, reduction_limit, alpha, beta);
-		}*/
 		return Evaluate(b);
 	}
 	u8 side = b.key >> 8 & 1ULL;
@@ -39,6 +27,10 @@ float alphabeta(position b, int depth, float alpha, float beta){
 		float val = INT_MAX;
 		Movelist  m;
 		movegen(&m, b);
+		//for(int i =0; i < m.Stack_size; i++) std::cout << (int)m.list[i].to << " "; std::cout << std::endl;
+		move_order(&b, &m);
+		//for(int i =0; i < m.Stack_size; i++) std::cout << (int)m.list[i].to << " " ; std::cout << std::endl;
+		//_sleep(456465);
 		for(int i=0; i < m.Stack_size; i++){
 			val = std::min(val, alphabeta(FromTo(b, m.list[i]), depth - 1, alpha, beta));
 			//ReadableBoard(FromTo(b, m.list[i]));
@@ -53,13 +45,14 @@ float alphabeta(position b, int depth, float alpha, float beta){
 
 }
 
-ScoredMove Next(position b, int depth){
+ScoredMove Next(position b, int depth){ // WTF ADD TO THERE A ALPHABETA OR SOMETHING LIKE THAT ????????
 	//printf("info depth %d\n",depth);
 	Node_Total = 0;
 	Move_Counter = b.key & 0b1111111;
 	int side = b.key >> 8 & 1ULL;
 	Movelist  m;
 	movegen(&m, b);
+	move_order(&b, &m);
 	std::vector<ScoredMove> ScoredMove_table;
 	float val = (side == WHITE) ? INT_MIN : INT_MAX;
 	float alp = 0;
@@ -102,3 +95,17 @@ ScoredMove Next(position b, int depth){
 }
 
 
+
+
+/*		const u8 side = (b.key >> 8 & 1ULL) ? WHITE : BLACK;
+		const u8 ENEMY_KING_SQUARE = (side == WHITE) ? Ls1bIndex(b.bk) : Ls1bIndex(b.wk);
+		const u8 ALLY_KING_SQUARE = (side == WHITE) ? Ls1bIndex(b.wk) : Ls1bIndex(b.bk);
+		//if(Node_Total > 450 && (is_square_attacked(b, ALLY_KING_SQUARE, (side)) || is_square_attacked(b, ENEMY_KING_SQUARE, SwapSide(side)))){
+			//ReadableBoard(b);
+			//_sleep(4500);
+		//}
+		//const u8 Current_Move = b.key & 0b1111111;
+		if( BitCheck(b.key, 33) == 0 && (is_square_attacked(b, ALLY_KING_SQUARE, side) || is_square_attacked(b, ENEMY_KING_SQUARE, SwapSide(side)) || BitCheck(b.key, 31)) ){
+			b.key |= 1ULL << 33;
+			return alphabeta(b, reduction_limit, alpha, beta);
+		}*/
